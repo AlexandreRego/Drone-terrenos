62501902e53b85b2871f3e95e8972023d2e7f43f
Alexandre Rego <102666174+AlexandreRego@users.noreply.github.com>
2024-11-29 15:35:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -85,8 +85,8 @@ from pathlib import Path
 
 @st.cache_resource
 def carrega_modelo():
-    #https://drive.google.com/file/d/1vRbnsbL4Wfwo7KdRTmZid0YI1rIKvw41/view?usp=drive_link
-    url = 'https://drive.google.com/uc?id=1vRbnsbL4Wfwo7KdRTmZid0YI1rIKvw41'
+    #https://drive.google.com/file/d/1mJ0nE-Cp7CU8U7xnOfMHC-U4UcRE2vsN/view?usp=drive_link
+    url = 'https://drive.google.com/uc?id=1mJ0nE-Cp7CU8U7xnOfMHC-U4UcRE2vsN'
     gdown.download(url, 'modelo_quantizado16bits.tflite', quiet=False)
     interpreter = tf.lite.Interpreter(model_path='modelo_quantizado16bits.tflite')
     interpreter.allocate_tensors()
