cedca5eb4abb690f8817dfae7b41c665a9f5e7d0
Alexandre Rego <102666174+AlexandreRego@users.noreply.github.com>
2024-12-07 18:44:16 -0300

    Update app.py
    retorno

--- app.py
+++ app.py
@@ -85,8 +85,8 @@ from pathlib import Path
 
 @st.cache_resource
 def carrega_modelo():
-    #https://drive.google.com/file/d/1BAzYOtcZcfnF-2YrPz4TukKVAUiRully/view?usp=drive_link
-    url = 'https://drive.google.com/uc?id=1BAzYOtcZcfnF-2YrPz4TukKVAUiRully'
+    #https://drive.google.com/file/d/1rN4RJyiB6gWa_F8dSpLYfQs7OZGFU978/view?usp=drive_link
+    url = 'https://drive.google.com/uc?id=1rN4RJyiB6gWa_F8dSpLYfQs7OZGFU978'
     gdown.download(url, 'modelo_quantizado16bits.tflite', quiet=False)
     interpreter = tf.lite.Interpreter(model_path='modelo_quantizado16bits.tflite')
     interpreter.allocate_tensors()
@@ -130,7 +130,7 @@ def previsao(interpreter, image):
     output_data = interpreter.get_tensor(output_details[0]['index'])
 
     # Classes do modelo
-    classes = ['terreno_erosao','terreno_represa', 'terreno_viavel']
+    classes = ['bom','erosao_represa']
     df = pd.DataFrame({'classes': classes, 'probabilidades (%)': 100 * output_data[0]})
     
     # Plotar gráfico de barras
